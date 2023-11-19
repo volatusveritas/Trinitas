cf406275b0d51f8c47ce1f315c489bf97946c1c0
Volatus <volatusCoder@outlook.com>
2023-11-19 18:51:49 -0300

    feat: draw id above playermouse

--- scenarios/TableScreen/PlayerMouse.gd
+++ scenarios/TableScreen/PlayerMouse.gd
@@ -1,8 +1,22 @@
 class_name PlayerMouse
 extends Control
 
-const MOUSE_RADIUS := 10.5
+const CIRCLE_RADIUS := 4.0
 const FONT_SIZE := 16.0
+const TEXT_MARGIN := -4.0
 
 func _draw() -> void:
-    draw_circle(Vector2.ZERO, MOUSE_RADIUS, Color.CORAL)
+    var font := get_theme_default_font()
+    var font_size := get_theme_default_font_size()
+
+    var text_width := font.get_string_size(
+        name,
+        HORIZONTAL_ALIGNMENT_CENTER,
+        -1,
+        font_size
+    )
+
+    var text_pos := Vector2(-text_width.x / 2, -text_width.y - TEXT_MARGIN)
+
+    draw_circle(Vector2.ZERO, CIRCLE_RADIUS, Color.CORAL)
+    draw_string(font, text_pos, name)
