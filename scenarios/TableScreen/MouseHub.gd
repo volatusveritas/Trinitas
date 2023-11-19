15fc7085e847c19192a9f1f2f648ded0a462c5c6
Volatus <volatusCoder@outlook.com>
2023-11-19 18:59:12 -0300

    refactor: move mousehub related code to mousehub

--- scenarios/TableScreen/MouseHub.gd
+++ scenarios/TableScreen/MouseHub.gd
@@ -1,6 +1,34 @@
 class_name MouseHub
 extends Control
 
+func _ready() -> void:
+    if multiplayer.is_server():
+        multiplayer.peer_connected.connect(share_mouse_list)
+        return
+
+    new_mouse.rpc(get_local_mouse_position())
+
+func _physics_process(delta: float) -> void:
+    update_mouse.rpc(get_local_mouse_position())
+
+func share_mouse_list(id: int) -> void:
+    var mouse_map := { "1": get_local_mouse_position() }
+
+    for child in get_children():
+        mouse_map[child.name] = child.position
+
+    receive_mouse_list.rpc_id(id, mouse_map)
+
+@rpc("any_peer", "call_remote", "reliable")
+func receive_mouse_list(mouse_map: Dictionary) -> void:
+    for id in mouse_map:
+        var player_mouse := PlayerMouse.new()
+
+        add_child(player_mouse)
+
+        player_mouse.name = id
+        player_mouse.position = mouse_map[id]
+
 @rpc("any_peer", "call_remote", "reliable")
 func new_mouse(pos: Vector2) -> void:
     var id := multiplayer.get_remote_sender_id()
@@ -14,6 +42,5 @@ func new_mouse(pos: Vector2) -> void:
 @rpc("any_peer", "call_remote", "unreliable")
 func update_mouse(new_pos: Vector2) -> void:
     var id := multiplayer.get_remote_sender_id()
-
     var player_mouse := get_node(str(id)) as PlayerMouse
     player_mouse.position = new_pos
