792e1f818b03e152c6753b33e623a1b9a5cc3f8f
Volatus <volatusCoder@outlook.com>
2023-11-21 19:01:34 -0300

    feat: add the TableItem class

---
+++ scripts/table_item.gd
@@ -0,0 +1,10 @@
+class_name TableItem
+extends Control
+
+var _path: String = ""
+
+func _get_syncinfo() -> Dictionary:
+    return {}
+
+func _apply_syncinfo(_syncinfo: Dictionary) -> void:
+    pass
