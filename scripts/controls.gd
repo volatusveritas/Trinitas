971efd918477b9ed87a5f4aaf8860190702cfb24
Volatus <volatusCoder@outlook.com>
2023-11-21 11:27:46 -0300

    refactor: move center_on_mouse's functionality to center_on

--- scripts/controls.gd
+++ scripts/controls.gd
@@ -1,11 +1,11 @@
-# Autoloaded: Controls
 class_name Controls
 extends Node
 
-static func center_on_mouse(obj: Control) -> void:
-    var mouse_pos = obj.get_global_mouse_position()
-
+static func center_on(obj: Control, pos: Vector2) -> void:
     obj.global_position = Vector2(
-        mouse_pos.x - obj.size.x / 2,
-        mouse_pos.y - obj.size.y / 2,
+        pos.x - obj.size.x / 2,
+        pos.y - obj.size.y / 2,
     )
+
+static func center_on_mouse(obj: Control) -> void:
+    center_on(obj, obj.get_global_mouse_position())
