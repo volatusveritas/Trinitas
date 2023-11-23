0232736e0254f387ad342eb9afd5f034eb568cbf
Volatus <volatusCoder@outlook.com>
2023-11-23 10:45:59 -0300

    feat: add updating interface for table items

--- scripts/table_item.gd
+++ scripts/table_item.gd
@@ -1,10 +1,17 @@
 class_name TableItem
 extends Control
 
-var _path: String = ""
+var _path := ""
 
 func _get_syncinfo() -> Dictionary:
     return {}
 
 func _apply_syncinfo(_syncinfo: Dictionary) -> void:
     pass
+
+func update_remote() -> void:
+    update_local.rpc(_get_syncinfo())
+
+@rpc("any_peer", "call_local", "reliable")
+func update_local(syncinfo: Dictionary) -> void:
+    _apply_syncinfo(syncinfo)
