e8acc435a80c336d97f7031c39a1963c57ca6b48
Volatus <volatusCoder@outlook.com>
2023-11-23 20:54:49 -0300

    feat: enable unfocusing any item with ESCAPE

--- scenarios/TableScreen/TableScreen.gd
+++ scenarios/TableScreen/TableScreen.gd
@@ -1,9 +1,19 @@
 extends Control
 
-@onready var NewItemSelector: NewItemSelector = $NewItemSelector
+@onready var NewItemSelector: NewItemSelector = $FixedLayer/NewItemSelector
 @onready var ItemStage: ItemStage = $ItemStage
 @onready var TableItems: TableItems = $TableItems
 
 func _ready() -> void:
     NewItemSelector.item_requested.connect(ItemStage.set_target)
     ItemStage.item_confirmed.connect(TableItems.create_item)
+
+func _input(input: InputEvent) -> void:
+    if not input is InputEventKey:
+        return
+
+    if input.pressed and input.keycode == KEY_ESCAPE:
+        var focused := get_tree().root.gui_get_focus_owner()
+
+        if focused != null:
+            focused.release_focus()
