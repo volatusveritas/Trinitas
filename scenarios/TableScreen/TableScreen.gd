6c7afa85979baf9bb9f8a7e23b7db76195cb78db
Volatus <volatusCoder@outlook.com>
2023-11-29 15:59:10 -0300

    feat: infinite background shaders
    - Camera smoothing was removed because it doesn't live update the actual
      camera position, thereby making it impossible to correctly synchronize
      the infinite background shader's offset with the camera's position. A
      custom camera smoothing feature will be added for greater control and
      to keep the smooth scrolling without losing the live updating
      characteristic which is necessary.

--- scenarios/TableScreen/TableScreen.gd
+++ scenarios/TableScreen/TableScreen.gd
@@ -1,12 +1,20 @@
 extends Control
 
-@onready var NewItemSelector: NewItemSelector = $FixedLayer/NewItemSelector
-@onready var ItemStage: ItemStage = $ItemStage
-@onready var TableItems: TableItems = $TableItems
+@onready var new_item_selector: NewItemSelector = $FixedLayer/NewItemSelector
+@onready var item_stage: ItemStage = $ItemStage
+@onready var table_items: TableItems = $TableItems
+@onready var infinite_background: ColorRect = $BackgroundLayer/InfiniteBackground
+@onready var camera: Camera2D = $Camera
 
 func _ready() -> void:
-    NewItemSelector.item_requested.connect(ItemStage.set_target)
-    ItemStage.item_confirmed.connect(TableItems.create_item)
+    new_item_selector.item_requested.connect(item_stage.set_target)
+    item_stage.item_confirmed.connect(table_items.create_item)
+
+func _physics_process(_delta: float) -> void:
+    (infinite_background.material as ShaderMaterial).set_shader_parameter(
+        "offset",
+        camera.position
+    )
 
 func _input(input: InputEvent) -> void:
     if not input is InputEventKey:
