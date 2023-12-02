8aa60f798faffe8cdf4962bbe51f64eaa5226dc4
Volatus <volatusCoder@outlook.com>
2023-12-02 03:11:44 -0300

    feat: add the Token table item

--- scenarios/TableScreen/NewItemSelector.gd
+++ scenarios/TableScreen/NewItemSelector.gd
@@ -18,6 +18,11 @@ func _ready() -> void:
         "res://scenes/table_items/SharedWriting/SharedWriting.tscn"
     )
 
+    add_item(
+        "Token",
+        "res://scenes/table_items/Token/Token.tscn"
+    )
+
 func _unhandled_input(_event: InputEvent) -> void:
     if Input.is_action_just_pressed("add_item"):
         visible = not visible
